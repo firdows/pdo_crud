5752c2f9e9faa19c4b12ffeea4067f4c42952f50
Jehdu Ahamad <ahamad.jedu@gmail.com>
2017-05-22 16:55:17 +0700

    สร้าง View แบบดึงหลายๆ แถว

--- index.php
+++ index.php
@@ -17,7 +17,41 @@ $db = $objDb->database;
 
      ?>
      <br/>
-     <a href="create">สร้าง Member</a>
+     <a href="create.php">สร้าง Member</a>
+     <br/>
+
+     <table border='1'>
+       <thead>
+         <tr>
+           <th>#</th>
+           <th>ชื่อ-สกุล</th>
+           <th>สถานะ</th>
+           <th>Action</th>
+         </tr>
+       </thead>
+       <tbody>
+<?php
+$query = $db->prepare("SELECT * FROM member");
+$query->execute();//ประมวลผลคำสั่ง sql
+
+
+if($query->rowCount() > 0){ #rowCount เช็คจำนวนแถวที่ได้มา
+  while($row = $query->fetch(PDO::FETCH_ASSOC)){//ดึงข้อมูลแต่ละรอบใส่ใน $row
+ ?>
+         <tr>
+           <td><?=$row['id']?></td>
+           <td><?=$row['firstname'].' '.$row['lastname']?></td>
+           <td><?=$row['status']?></td>
+           <td>
+            <a href="#">ดู</a> | <a href="#">แก้ไข</a> | <a href="#">ลบ</a>
+           </td>
+         </tr>
+<?php
+  }#while
+}
+?>
+       </tbody>
+     </table>
 
   </body>
 </html>
